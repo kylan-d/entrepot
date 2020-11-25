06ad5c1d8049db07e203c4abf4e7f4712c67fc12
kylan-d <73824522+kylan-d@users.noreply.github.com>
2020-11-25 15:43:19 +0100

    licenciement

--- Chef.java
+++
@@ -1,7 +0,0 @@
-package projet;
-public abstract class Chef extends Personne{
-    Ouvrier liste_ouv[]= new Ouvrier[4];
-    int tailleeq;
-    //recruter_ouvrier(){}
-    //licencier_ouvrier(){}
-}
--- Personne.java
+++
@@ -1,8 +0,0 @@
-package projet;
-public abstract class Personne{
-    String Nom;
-    String Prenom;
-    boolean actif;
-    int i;
-    static int id;
-}
