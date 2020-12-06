7a479a92dac04b777af9421e2be5c288d7556f49
Abdesamad Meziane <abdesamad.meziane.pro@hotmail.com>
2020-12-06 20:59:08 +0100

    Modif classe Meuble

--- Meuble.java
+++ Meuble.java
@@ -2,7 +2,7 @@ package projet;
 
 import java.util.ArrayList;
 public class Meuble{
-    double prix;  // Je pense qu'il est inutile de rajoute cette variable. Etant donné Qu'une liste de piece est composé de piece. Il suffirait de faire un fonction qui fait la somme des prix des pieces pour le prix d'un lots. Puis pour un meuble Si il a besoins de plusieurs lot, on fait la somme du prix de chaques lots.
+    double prix;  
     ArrayList<LotPiece> liste_lot_piece;
     String piece;
     int duree;
